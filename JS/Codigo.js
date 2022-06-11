5cd051928085bdc5d075a02ec02bb9a284fbfb17
TheCodeGuy2000 <107319472+TheCodeGuy2000@users.noreply.github.com>
2022-06-11 14:21:48 -0500

    Add files via upload

---
+++ JS/Codigo.js
@@ -0,0 +1,9 @@
+
+//Imprimir saludo al entrar a la pagina
+
+let nombre = prompt ("Hola purificador, ¿Como te llamas?");
+
+alert ("Nos alegra tenerte con nosotros " + nombre);
+
+//--------------------------------------------------------//
+
